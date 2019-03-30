23195a1a5778da7d4321c80b225440d2264195e9
Aditya Chavan <adityachavan198@gmail.com>
2019-03-30 14:57:39 +0530

    second commit

--- MoneyMe/Money/urls.py
+++ MoneyMe/Money/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls import url
 urlpatterns=[
-    path('', views.index, name='libraryindex'),
+    # path('', views.index, name='index'),
+    path('summary/', views.summary, name='Summary')
+
 ]
